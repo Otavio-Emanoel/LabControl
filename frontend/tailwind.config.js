6d6cc6d1468065d6856a13c02313514f51f847ce
Everton Nascimento <124212111+Evy8882@users.noreply.github.com>
2025-08-26 14:23:43 -0300

    Homepage e navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,43 +3,26 @@ module.exports = {
   content: [
     "./App.tsx",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}"
+    "./app/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
         primary: {
-          DEFAULT: '#1F41BB',   // roxo principal
+          DEFAULT: '#1F41BB',
           light: '#1F41BB',
           dark: '#1F41BB',
-        },
-        secondary: {
-          DEFAULT: '#03DAC6',   // verde água
-          light: '#66FFF9',
-          dark: '#00A896',
-        },
-        background: {
-          DEFAULT: '#FFFFFF',
-          dark: '#121212',
-        },
-        surface: {
-          DEFAULT: '#F5F5F5',
-          dark: '#1E1E1E',
-        },
-        text: {
-          primary: '#000000',
-          secondary: '#666666',
-          inverse: '#FFFFFF',
-          muted: '#A1A1AA',
-        },
-        border: {
-          DEFAULT: '#E5E5E5',
         },
         danger: '#B00020',
         warning: '#FFC107',
         success: '#4CAF50',
         info: '#2196F3',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        madimi: ['"Madimi One"', 'cursive'],
       },
     },
   },
