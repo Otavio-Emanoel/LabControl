0e58eed5da3ff1e03e0541d9deda549d8ae158ef
= <evy1339@gmail.com>
2025-08-30 20:20:47 -0300

    Adiciona imagem de fundo e botões de seleção na página de agendamento; atualiza o componente LabCard para exibir o nome do laboratório formatado; Corrige conexão API

--- frontend/app/agendamento.tsx
+++ frontend/app/agendamento.tsx
@@ -1,13 +1,16 @@
 import { View, ScrollView } from 'react-native';
 import axios from 'axios';
+import { Image, Text, TouchableOpacity } from 'react-native';
 import { useState, useEffect } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 import LabCard from '@/components/labCard';
 
 interface Lab {
-  nome: string;
+  numero: string;
 }
 
 export default function AgendamentoPage() {
+  const [selected, setSelected] = useState<'professores' | 'organizacao'>('professores');
   const [labs, setLabs] = useState<Lab[]>([]);
 
   useEffect(() => {
@@ -23,10 +26,32 @@ export default function AgendamentoPage() {
   }, []);
 
   return (
-    <ScrollView>
+    <ScrollView className='flex-1 bg-black'>
+      {/* background image */}
+      <View style={{ width: '100%', height: '33%', position: 'absolute', top: 0, left: 0 }}>
+        <Image source={require("../assets/images/bg2.jpg")} style={{ width: '100%', height: '100%' }} />
+      </View>
+        <View className='flex-1 flex-row items-center h-full p-16 z-10 font-poppins'>
+            <Ionicons name="arrow-back" size={16} color="white" className="mr-4" />
+            <Text className='color-white text-xl'>Voltar</Text>
+        </View>
+        <View className='flex-row items-center mx-auto rounded-full h-8 z-10 bg-white w-[90%] items-center justify-center'>
+            <TouchableOpacity 
+              className={`w-[50%] h-8 flex items-center justify-center rounded-full ${selected === 'professores' ? 'bg-[#3B96E2]' : ''}`} 
+              onPress={() => setSelected('professores')}
+            >
+              <Text className={`font-poppins ${selected === 'professores' ? 'color-white' : ''}`}>Professores</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              className={`w-[50%] h-8 flex items-center justify-center rounded-full ${selected === 'organizacao' ? 'bg-[#3B96E2]' : ''}`} 
+              onPress={() => setSelected('organizacao')}
+            >
+              <Text className={`font-poppins ${selected === 'organizacao' ? 'color-white' : ''}`}>Organização</Text>
+            </TouchableOpacity>
+        </View>
       <View>
         {labs.map((lab, index) => (
-          <LabCard labName={lab.nome} key={index} />
+          <LabCard labName={lab.numero} key={index} />
         ))}
       </View>
     </ScrollView>
--- frontend/components/labCard.tsx
+++ frontend/components/labCard.tsx
@@ -1,8 +1,8 @@
 import { View, Text } from 'react-native'
 
 export default function LabCard({labName}: {labName: string}) {
-    return (<View className='flex w-[90%] h-full bg-blue-950 rounded-2xl mx-auto my-4 p-4'>
+    return (<View className='flex w-[90%] h-20 bg-[#01071d] rounded-2xl mx-auto my-4 p-4'>
         {/* Cards de laboratórios */}
-        <Text className='text-white font-bold text-2xl'>{labName}</Text>
+        <Text className='text-white font-bold text-xl'>{labName.charAt(0).toUpperCase() + labName.slice(1)}</Text>
     </View>)
 }
