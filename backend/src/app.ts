fffe2ca44ecff86b0d8de0f8ef7fd3512ef6f2ce
João <jaoetec24@gmail.com>
2025-08-30 18:37:58 -0300

    Implementa o CORS no backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import testRouter from './test/test.router';
 import authRouter from './routes/auth.routes';
@@ -8,6 +9,9 @@ import labsRouter from './routes/labs.routes';
 import { pool } from './database/connection';
 
 const app = express();
+
+// habilita o cors (config simples: permite tudo)
+app.use(cors());
 
 app.use(express.json());
 
